a54b83fce8bbc2b7733b1a20db912e31555d0fc5
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-10 04:26:14 +0530

    add users shema

--- models/Book.js
+++ models/Book.js
@@ -1,1 +1,17 @@
-console.log("ouwghlsjn");
+// console.log("ouwghlsjn");
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const blogSchema = new Schema({
+  title: String, // String is shorthand for {type: String}
+  author: String,
+  genre: String,
+  publishedYear: [{ body: String, min:1000 }],
+  price: { type: Number, mime:0 },
+  inStock{type:Boolean,default:true}
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number,
+  },
+});
