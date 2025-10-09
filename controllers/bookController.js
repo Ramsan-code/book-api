ce8e3c432f2773fb0e90a1b73a67fe281f7184a5
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-10 05:05:32 +0530

    work get methode ,get all

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,1 +1,11 @@
-console.log("eigjrsnkf");
+// console.log("eigjrsnkf");
+import Book from "../models/Book.js";
+
+export const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
