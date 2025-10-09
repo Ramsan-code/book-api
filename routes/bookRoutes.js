86d859f1171c99b79f805c40a11d6ec0cc99636f
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-10 04:39:59 +0530

    create bookRoutes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,1 +1,18 @@
-console.log("gokfsl");
+// console.log("gokfsl");
+import express from "express";
+import {
+  getAllBooks,
+  getBooksById,
+  createBook,
+  updateBook,
+  deleteBook,
+} from "../controllers/bookController.js";
+
+const router = express.Router();
+router.get("/", getAllBooks);
+router.get("/:id", getBooksById);
+router.post("/:id", createBook);
+router.get("/", updateBook);
+router.get("/:id", deleteBook);
+
+export default router;
