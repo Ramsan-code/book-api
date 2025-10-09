4160002f482873f014a38bf6a9f7222d02eaae2e
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-09 20:30:46 +0530

    not work then function error thorw

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     await mongoose.connect("mongodb://127.0.0.1/my_database");
     console.log(" mongodb Connected! ");
   } catch (error) {
-    console.log("error connection!");
+    console.error("error connection!", error);
   }
 };
 connectDB();
