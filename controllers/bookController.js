65078a09625f9ac58a6feaf76aba31b795e0844f
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-12 21:20:26 +0530

    Add basic error handling

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -16,16 +16,23 @@ export const getBooksById = async (req, res) => {
   try {
     const bookID = req.params.id;
     const book = await Book.findById(bookID);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
     res.json(book);
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 //<-------------Add a new book to the database -------->
 export const createBook = async (req, res) => {
   try {
     const book = await Book.create(req.body);
-    res.json(book);
-  } catch (error) {}
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 //<---------------Update a book information--------->
@@ -37,8 +44,13 @@ export const updateBook = async (req, res) => {
       new: true,
       runValidators: true,
     });
+    if (!updateBook) {
+      return res.status(404).json({ message: error.message });
+    }
     res.json(updateBook);
-  } catch (error) {}
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 //<---------------Delete a book from the database----->
@@ -47,6 +59,11 @@ export const deleteBook = async (req, res) => {
   try {
     const bookID = req.params.id;
     const deleteBook = await Book.findByIdAndDelete(bookID);
-    res.json(deleteBook);
-  } catch (error) {}
+    if (!deleteBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res.json({ message: "deleted Book" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
