d9ffc88f1eb980d666c6d83656d07b57f288baf2
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-12 15:46:49 +0530

    Correct my database name

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 // console.log("ef");
-
+import express from "express";
+import bookRoutes from "./routes/bookRoutes.js";
 import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/my_database");
+    await mongoose.connect("mongodb://127.0.0.1:27017/bookdb");
     console.log(" mongodb Connected! ");
   } catch (error) {
     console.error("error connection!", error);
   }
 };
+
 connectDB();
-import express from "express";
+
 const app = express();
 const PORT = 2000;
 
 app.use(express.json());
-import bookRoutes from "./routes/bookRoutes.js";
 
 app.use("/api/books", bookRoutes);
+
 app.get("/", (req, res) => {
   res.send("HI! EXPRESS");
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
